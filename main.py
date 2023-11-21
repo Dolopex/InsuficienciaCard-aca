d7a5357fd746d048ae89af765d6a14049e59d32f
Dolopex <dolopex2004@hotmail.com>
2023-11-21 14:32:54 -0500

    Parte 3

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
 from datasets import load_dataset
 import numpy as np
+import pandas as pd
 
 # Se cargan los datos
 dataset = load_dataset("mstz/heart_failure")
 data = dataset["train"]
 
-# Obtiene la lista de edades
-edades = data["age"]
+# Convierte la estructura en dataframe de Pandas
+df = pd.DataFrame(data)
 
-# Convierte la lista de edades a un arreglo de numpy
-edades_np = np.array(edades)
+# Separa el dataframe en perecidos y sobrevivientes
+df_perecidos = df[df['is_dead'] == 1]
+df_sobrevivientes = df[df['is_dead'] == 0]
 
-# Calcula el promedio de las edades de los pacientes
-promedio_edad = np.mean(edades_np)
+# Calcula el promedio de las edades de los pacientes de cada dataset
+promedio_edad_perecidos = np.mean(df_perecidos['age'])
+promedio_edad_sobrevivientes = np.mean(df_sobrevivientes['age'])
 
 # Imprime el resultado
-print(f"El promedio de edad de las personas participantes en el estudio es: {promedio_edad:.2f}")
+print(f"El promedio de edad de las personas que perecieron es: {promedio_edad_perecidos:.2f}")
+print(f"El promedio de edad de las personas que sobrevivieron es: {promedio_edad_sobrevivientes:.2f}")
+
+"""Verificar si los datos son correctos"""
+tipos_de_datos = df.dtypes
+print("Tipos de datos en cada columna:")
+print(tipos_de_datos)
+
+# Calcular la cantidad de hombres fumadores vs mujeres fumadoras
+# usando la función groupby agregamos género y hábito de fumar
+conteo_fumadores = df.groupby(['is_male', 'is_smoker']).size().unstack().fillna(0)
+
+# Mejorar la presentación del resultado
+conteo_fumadores.columns = ['No Fumador', 'Fumador']
+conteo_fumadores.index = ['Mujer', 'Hombre']
+
+# Imprimir el resultado
+print("\nCantidad de hombres y mujeres fumadoras:")
+print(conteo_fumadores)
--- readme.md
+++ readme.md
@@ -23,3 +23,21 @@ pip install datasets
 1.4 Se crea una variable que calcule el promedio de las edades
 
 1.5 Se imprime el resultado
+
+## Notas Parte 2
+2.0 Se importa Pandas
+
+2.1 Se Cambia el Dataset de numpy a Pandas con pd.DataFrame
+
+2.2 Se Separa el DataFrame en 2 para calcular el promedio de cada uno, se le asigna el valor de 1 a los pacientes perecidos y 0 a los sobrevivientes
+
+2.3 Se calcula el promedio de las edades de los 2 dataframes con numpy
+
+## Notas Parte 3
+3.0 Se verifican los tipos de datos de cada columna del DataSet y se imprimen los resultados
+
+3.1 Se crea una variable que usando la funcion Groupby se agregan los datos del genero y sin son fumadores o no
+
+3.2 Como agregado personal ajuste las columnas y los indices para que la informacion tenga una mejor presentacion
+
+3.3 Se imprimen los resultados
